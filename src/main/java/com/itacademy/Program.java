39dd425e3319d5e4650c3708e3ca4c109d564b7c
WINDOWS 10 <zivkovicjelena1997@gmail.com>
2025-11-19 18:38:00 +0100

    Izmjenjen Program.java i dodan ThreeDevo

--- src/main/java/com/itacademy/Program.java
+++ src/main/java/com/itacademy/Program.java
@@ -4,5 +4,6 @@ package com.itacademy;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Pozdrav raja kako ste");
+        System.out.println("Jeste mi dobro, kako ste?");
     }
 }
---
+++ src/main/java/com/itacademy/three/ThreeDemo.java
@@ -0,0 +1,4 @@
+package com.itacademy.three;
+
+public class ThreeDemo {
+}
