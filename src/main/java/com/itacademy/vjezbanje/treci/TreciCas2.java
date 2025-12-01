f94941a78f50a8a2e65e580bae996a2da047f90c
WINDOWS 10 <zivkovicjelena1997@gmail.com>
2025-12-01 01:40:49 +0100

    Zadatci za grananja, petlje, pitanja

--- src/main/java/com/itacademy/vjezbanje/treci/TreciCas2.java
+++ src/main/java/com/itacademy/vjezbanje/treci/TreciCas2.java
@@ -1,13 +1,31 @@
 package com.itacademy.vjezbanje.treci;
 
+import java.util.Scanner;
+
 public class TreciCas2 {
     public static void main(String[] args) {
-       char slovo = 's';
-        System.out.println("Slovo = " + slovo );
-        char slovo2 = 65;
-        System.out.println("Slovo = " + slovo2);
-        int number = slovo2;
-        System.out.println("Number =" + number);
+         int x =20;
+         if(x=20)
+             System.out.println(x);
+
+
+        }
+
+
+
+        }
+
 
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/itacademy/vjezbanje/treci/cetvrti/Cetvrti3.java
+++ src/main/java/com/itacademy/vjezbanje/treci/cetvrti/Cetvrti3.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Cetvrti3 {
     public static void main(String[] args) {
-
+        int number = 23;
+        number ++;
+        System.out.println(number);
+        ++number;
+        System.out.println(number);
+        int broj= 24;
+        System.out.println(broj--);
+        System.out.println(--broj);
+        boolean success= true;
+        System.out.println( !success);
     }
-   }
+}
+
+
+
+
+
+
